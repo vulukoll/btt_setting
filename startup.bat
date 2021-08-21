2ceab98857cfc62c539c5f71eca4661b8b069a3e
vikukoll <88854363+vulukoll@users.noreply.github.com>
2021-08-21 00:13:27 +0000

    Add files via upload

---
+++ startup.bat
@@ -0,0 +1,17 @@
+
+timeout /t 60
+cd C:\install\torrent-autoremove-tool-master
+
+echo installing dependencies...
+cd app
+pip install -r requirements.txt
+
+TIMEOUT /T 120 /NOBREAK 
+cd app
+python main.py
+
+echo install and run block
+
+cd C:\install\torrent-manager-main
+TIMEOUT /T 120 /NOBREAK
+npm i && node src/index.js
